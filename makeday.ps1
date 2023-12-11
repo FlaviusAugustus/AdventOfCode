9d1165bb37dae6b522bba96ed92f2e6dee4c647c
gracjan grzech <g.grzech@icloud.com>
2023-12-11 19:10:24 +0100

    Added automatic day file generation

---
+++ makeday.ps1
@@ -0,0 +1,13 @@
+﻿param(
+    [ValidateRange(2015, 2100)]
+    [string]$Year = (Get-Date).year.ToString(),
+    [ValidateRange(1, 25)]
+    [string]$Day = (Get-Date).day.ToString()
+)
+
+$dayString = "Day$($Day.PadLeft(2,'0'))"
+$filePath = ".\$($Year)\$($dayString).cs"
+$baseFilePath = ".\dayBase"
+
+(Get-Content $baseFilePath).Replace('[_YEAR]', "_$($Year)") | Set-Content $filePath
+(Get-Content $filePath).Replace('[DAY]', "$($dayString)") | Set-Content $filePath
